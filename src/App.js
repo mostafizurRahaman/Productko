118297d103a878b77a0960a2a7c33c177b7c01da
mostafizurRahaman <fahim654326@gmail.com>
2022-11-24 00:37:10 +0600

    navbar is designed and create responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import { RouterProvider } from 'react-router-dom';
+import Routes from './Route/Routes/Routes'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        
+        <RouterProvider router={ Routes }>
+
+        </RouterProvider>
     </div>
   );
 }
